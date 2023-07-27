8fbb17488157ed68e9bead07b27ae99f79125d26
Jeremy Custenborder <jcustenborder@gmail.com>
2023-07-27 15:25:35 -0500

    Enable logging.

--- generate-releasenotes.sh
+++ generate-releasenotes.sh
@@ -1,10 +1,11 @@
 #mvn org.apache.maven.plugins:maven-dependency-plugin:2.1:get -DrepoUrl=https://download.java.net/maven/2/  -Dartifact=se.bjurr.gitchangelog:git-changelog-command-line:1.103.2
-java -jar  ~/.m2/repository/se/bjurr/gitchangelog/git-changelog-command-line/1.103.2/git-changelog-command-line-1.103.2.jar -sf ./changelog.json -ini -ge -std -tec "
+java -jar -Dorg.slf4j.simpleLogger.defaultLogLevel=trace ~/.m2/repository/se/bjurr/gitchangelog/git-changelog-command-line/1.103.2/git-changelog-command-line-1.103.2.jar -sf ./changelog.json -std -tec "
 # Changelog
 
 Changelog.
-{{#issues}}
-  {{  . }}
-{{/issues}}
 
+{{#issues}}
+  {{ . }}
+  {{ type }}
+{{/issues}}
 "
