84af5331c57d3b795201db3453ddbd41d5317372
Jeremy Custenborder <jcustenborder@gmail.com>
2023-07-31 10:26:31 -0500

    Move to 1.104.1 of the command line library for the ifContainsIssueLabel helper. Closes #3

--- generate-releasenotes.sh
+++ generate-releasenotes.sh
@@ -1,11 +1,25 @@
-#mvn org.apache.maven.plugins:maven-dependency-plugin:2.1:get -DrepoUrl=https://download.java.net/maven/2/  -Dartifact=se.bjurr.gitchangelog:git-changelog-command-line:1.103.2
-java -jar -Dorg.slf4j.simpleLogger.defaultLogLevel=trace ~/.m2/repository/se/bjurr/gitchangelog/git-changelog-command-line/1.103.2/git-changelog-command-line-1.103.2.jar -sf ./changelog.json -std -tec "
-# Changelog
+mvn org.apache.maven.plugins:maven-dependency-plugin:2.1:get -DrepoUrl=https://download.java.net/maven/2/  -Dartifact=se.bjurr.gitchangelog:git-changelog-command-line:1.104.1
+java -jar -Dorg.slf4j.simpleLogger.defaultLogLevel=trace ~/.m2/repository/se/bjurr/gitchangelog/git-changelog-command-line/1.104.1/git-changelog-command-line-1.104.1.jar -sf ./changelog.json -std -tec "
+# Release Notes
 
-Changelog.
+{{#ifContainsIssueLabel issues label='enhancement'}}
+## Enhancements
 
 {{#issues}}
-  {{ . }}
-  {{ type }}
+{{#ifIssueLabel . label='enhancement'}}
+* [{{ issue }} - {{ title}}]({{ link }})
+{{/ifIssueLabel}}
 {{/issues}}
+{{/ifContainsIssueLabel}}
+
+{{#ifContainsIssueLabel issues label='bug'}}
+## Bugs
+
+{{#issues}}
+{{#ifIssueLabel . label='bug'}}
+* [{{ issue }} - {{ title}}]({{ link }})
+{{/ifIssueLabel}}
+{{/issues}}
+{{/ifContainsIssueLabel}}
 "
+
